842b0a0a93f97849676b9645527af73de39fed01
JGSowmya <jgsbhat@gmail.com>
2019-12-08 20:49:38 +0530

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Sowmya J G on 08/12/19.
+//  Copyright © 2019 Sowmya J G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
